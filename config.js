9b159a18b7917f3878d691df6bf167654a75a5e5
Aku Vins <70185612+aku-vins@users.noreply.github.com>
2020-09-26 04:17:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	prefix: "sonic",
 	owners: ["727110220400033865"],
-	token: "NzQ3NDMzNDM4NjI5MzMwOTk2.X0Ozog.Qud23zO7n3ZZoU4YB35bWTUKSGk",
+	token: "your token",
 };
+
+/* this project from SherifPoetra
